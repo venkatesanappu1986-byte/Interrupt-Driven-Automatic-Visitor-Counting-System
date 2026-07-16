dd7ad0b4806d1c7ffe52173cfe49ff9ead9a9f44
venkatesanappu1986-byte <venkatesanappu1986@gmail.com>
2026-07-16 09:28:11 +0530

    Update miniproject.c

--- miniproject.c
+++ miniproject.c
@@ -1,122 +1,106 @@
-/* AUTOMATIC VISITOR COUNTING OPERATING SYSTEM */
-
-#include <lpc21xx.h>
-#include "header.h"
-
-#define EINT0   (1 << 0)
-#define EINT1   (1 << 29)
-#define LED     (1 << 10)
-#define BUZZER  (1 << 11)
-#define SWITCH  17
-
-volatile unsigned int lcd_count = 0, total_entry = 0, total_exit = 0;
-volatile unsigned int flag1 = 0, flag2 = 0;
-
-void entry_person(void) __irq 
-{
-        IOCLR0 = LED;
-        flag1 = 1;
-        EXTINT = 0x01;
-        VICVectAddr = 0;
+#include<lpc21xx.h>
+#include"lcd_header.h"
+#define  LED 1<<12
+#define  BUZZER 1<<13
+#define SW 17
+volatile unsigned int flag1=0;
+volatile unsigned int flag2=0;
+void LCD_DISPLAY(void);
+unsigned int ENTRY=0;
+unsigned int EXIT=0;
+int count=0;
+ void ext0__into__isr(void)__irq
+ {
+    IOCLR0=LED;
+    EXTINT=0X01;
+	flag1=1;
+	VICVectAddr=0;
+ }
+ void ext1__into__isr(void)__irq
+ {
+    EXTINT=0X02;
+	flag2=1;
+	VICVectAddr=0;
+ }
+ int main()
+ {
+   PINSEL0|=0x20000000;
+   PINSEL1|=0X01;
+   IODIR0|=LED|BUZZER|LCD_D|RS|E;
+   LCD_INIT();
+   LCD_DISPLAY();
+   
+   
+   VICIntSelect=0;
+   VICVectCntl0=(1<<5)|14;
+   VICVectAddr0=(unsigned long)ext0__into__isr;
+   VICVectCntl1=(1<<5)|15;
+   VICVectAddr1 =(unsigned long)ext1__into__isr;
+   EXTMODE=0x00;
+   EXTPOLAR=0x00;
+   VICIntEnable=(1<<14)|(1<<15);
+   while(1)
+   {
+   if(flag1)
+   {  
+   ++count;
+   ++ENTRY;
+   
+   IOCLR0=BUZZER;
+   delay_millisecond(500);
+   IOSET0=BUZZER;
+   delay_millisecond(500);
+   flag1=0;
+   }
+   if(flag2)
+   {
+   if(count>0)
+   {  
+   count--;
+   EXIT++;
+   IOCLR0=BUZZER;
+ delay_millisecond(500);
+ IOSET0=BUZZER;
+ delay_millisecond(500);
+   }
+   flag2=0;
+   }
+   
+   if(((IOPIN0>>SW)&1)==0)
+   {
+     LCD_COMMAND(0X01);
+     LCD_COMMAND(0X80);
+     LCD_STR("VISITOR ENTRY:");
+     LCD_INTEGER(ENTRY);
+     LCD_COMMAND(0XC0);
+     LCD_STR("VISITOR EXIT:");
+     LCD_INTEGER(EXIT);
+     delay_millisecond(3000);
+     LCD_COMMAND(0X01);
+   }
+   if((flag1==0)&&(flag2==0))
+   {
+     if(count>0)
+     {
+     IOCLR0=LED;
+     }
+     else
+     {
+     IOSET0=LED;
+     }
+     LCD_COMMAND(0X80);
+     LCD_STR("VISITOR COUNT");
+     LCD_COMMAND(0XC6);
+     LCD_INTEGER(count);
+     delay_millisecond(500);
+     }     
+   }
 }
-
-void exit_person(void) __irq 
+void LCD_DISPLAY()
 {
-        flag2 = 1;
-        EXTINT = 0x02;
-        VICVectAddr = 0;
-}
-
-void display(void) 
-{
-        lcd_command(0x01); 
-        if (lcd_count == 0)
-               {
-                lcd_command(0x84);
-                lcd_str("NO VISITOR");
-                IOSET0 = LED;
-        }
-               else 
-        {
-                lcd_command(0x80);
-                lcd_str(lcd_count == 1 ? "VISITOR:" : "VISITORS:");
-                lcd_command(lcd_count == 1 ? 0x89 : 0x8A);
-                lcd_integer(lcd_count);
-                IOCLR0 = LED;
-        }
-}
-
-void buzzer(void) 
-{
-        IOCLR0 = BUZZER;
-        delay_sec(1);
-        IOSET0 = BUZZER;
-        delay_ms(1);
-}
-
-int main() 
-{
-        PINSEL0 |= EINT1;
-        PINSEL1 |= EINT0;
-        IODIR0 = LED | BUZZER;
-        lcd_init();
-
-        VICIntSelect = 0;
-        VICVectCntl0 = (1 << 5) | 14;
-        VICVectAddr0 = (unsigned long)entry_person;
-
-        VICVectCntl1 = (1 << 5) | 15;
-        VICVectAddr1 = (unsigned long)exit_person;
-
-        EXTMODE = 0x00;
-        EXTPOLAR = 0x00;
-
-        lcd_command(0x81); 
-	      lcd_str("AUTOMATIC VISITOR");
-        lcd_command(0xC2); 
-	      lcd_str("COUNTING SYSTEM");
-        delay_ms(2);
-
-        EXTINT = 0x03;
-        IOSET0 = BUZZER | LED; 
-        VICIntEnable = (1 << 14) | (1 << 15);    
-
-        display(); 
-
-        while (1) {
-                if (flag1) 
-                {  
-                        lcd_count++;
-                        total_entry++;
-                        display();
-                        buzzer();
-                        flag1 = 0;
-                } 
-                else if (flag2) 
-                { 
-                        if (lcd_count > 0) 
-                                lcd_count--;
-                        total_exit++;
-                        display();
-                        buzzer();
-                        flag2 = 0;
-                } 
-                else if (((IOPIN1 >> SWITCH) & 1) == 0)
-                { 
-                        delay_ms(500); 
-                        lcd_command(0x01);
-                        lcd_command(0x82);  
-									      lcd_str("TOTAL ENTERED:");
-                        lcd_command(0x91); 
-				       					lcd_integer(total_entry);
-                        lcd_command(0xC2);  
-								      	lcd_str("TOTAL EXITED:");
-                        lcd_command(0xD1); 
-									      lcd_integer(total_exit);
-
-
-                        while(((IOPIN1 >> SWITCH) & 1) == 0);
-                        display();
-                }
-        }
+  LCD_INIT();
+  LCD_COMMAND(0X80);
+  LCD_STR("INTERRUPT DRIVEN    VISITOR COUNT SYSTEM");
+  delay_second(2);
+  LCD_COMMAND(0X01);
 }
